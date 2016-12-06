6c8512ea95439d14629a80cc64b6ea7e26ef9a16
zibomail <zibomail@gmail.com>
2016-12-07 07:09:24 +0800

    homepc

--- lizi/zuoyedaima.py
+++ lizi/zuoyedaima.py
@@ -8,9 +8,13 @@ class Ren:
 
     xuexidict = {"烤肉串":[500,10],"新东方厨师":[8000,20],"SAP培新":[50000,50],"老男孩Python":[5800,50]}
 
+    fangzidict = {"北三环60平米":50000,"南三环50平米":40000,"东三环40平米":20000}
+
+    chedict = {"吉利":50000,"卡罗拉":100000,"斯巴鲁":200000,"特斯拉":1000000}
 
 
-    def __init__(self,xingbie,nianling,gongzuo,cunkuan,jingli,fangzi,che):
+
+    def __init__(self,xingbie,nianling,gongzuo,cunkuan,jingli=100,fangzi=0,che=0):
 
         self.xingbie = xingbie
         self.nianling = nianling
@@ -22,5 +26,6 @@ class Ren:
 
 
     def  work(self):
+
 
 
